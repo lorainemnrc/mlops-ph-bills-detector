ae89b294a9b4a1c4f923831ed2f6a9c381de6a48
Loraine <LMenorca@outlook.com>
2023-09-19 22:16:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,8 +137,8 @@ def main():
                 img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
 
                  # Define the new dimensions (width, height)
-                new_width = 500  # Adjust to your desired width
-                new_height = 300  # Adjust to your desired height
+                new_width = 640  # Adjust to your desired width
+                new_height = 640  # Adjust to your desired height
             
                 # Resize the image
                 img = cv2.resize(img, (new_width, new_height))
